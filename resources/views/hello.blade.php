697b543341e5d665c73636fc3b209fd8bb2bd674
PetkoPopov <poetpoet2014@mail.ru>
2020-11-13 21:57:57 +0200

    new laravel projrct

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <body style="background-color: #ffcc33">
+        <form>
+            <input type="text" name="hello"/>
+            <input type="submit"/>
+        
+    </form>
+        
+        
+    </body>
+    
+    
+</html>
